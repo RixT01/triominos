b32ce0ba107c10508fabe87c81a77ac98e513165
Unknown <rixtrevizo@gmail.com>
2018-02-03 15:47:46 -0700

    init mix

---
+++ lib/fit_triominos.ex
@@ -0,0 +1,18 @@
+defmodule FitTriominos do
+  @moduledoc """
+  Documentation for FitTriominos.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FitTriominos.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fit_triominos_test.exs
@@ -0,0 +1,8 @@
+defmodule FitTriominosTest do
+  use ExUnit.Case
+  doctest FitTriominos
+
+  test "greets the world" do
+    assert FitTriominos.hello() == :world
+  end
+end
